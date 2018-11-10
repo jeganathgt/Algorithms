bef2519fbc13e50fb37dba8748c31386462dee37
jeganath <gtjeganath@gmail.com>
2018-11-10 13:05:50 +0530

    print the sering using subtracing each number by 5 until 0

---
+++ print_series_recursion/main_3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+#define INTERVAL_SIZE 5
+
+int print_number (int number, int interval) {
+
+    printf("%d ", number);
+
+    if(number < 0)
+        return 0;
+
+    print_number(number-interval, interval);
+
+    printf("%d ", number);
+
+    return 0;
+}
+
+
+int main()
+{
+    int  number   = 0;
+
+    printf("Enter the number:");
+    scanf("%d", &number);
+
+
+    print_number(number, INTERVAL_SIZE);
+    printf("\n");
+    return 0;
+}
